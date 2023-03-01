b0dd1adaed09bea9779f30be2d7dea5d1763daa8
Filipe Costa <filpemdc2@gmail.com>
2023-03-01 00:50:56 -0300

    Navbar, and more

---
+++ components/Loader.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Loader({ show }: { show: boolean }) {
+  return (
+    show ? (
+      <div className="loader">
+      </div>
+    ) : null
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 
+import Loader from '../../components/Loader'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -16,6 +18,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Loader show={true} />
       <div>
         <Link href={{
           pathname: '[username]',
