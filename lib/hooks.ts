aefff4f2b775374eb5ed18c59b143e532fec7c17
Filipe Costa <filpemdc2@gmail.com>
2023-03-01 22:11:46 -0300

    hooks updated to onsnapshot, enter done with username batch/commit in firestore and verify if exists

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,4 +1,4 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, onSnapshot } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, firestore } from "./firebaseInit";
@@ -13,17 +13,33 @@ export function useUserData() {
     }
   }, [user]);
 
-  async function getUserData(user: any) {
-    const docRef = doc(firestore, "users", user.uid);
-    const docSnap = await getDoc(docRef);
+  // async function getUserData(user: any) {
+  //   const docRef = doc(firestore, "users", user.uid);
+  //   const docSnap = await getDoc(docRef);
 
-    if (docSnap.exists()) {
-      setUsername(docSnap.data()?.username);
-    } else {
-      setUsername(null);
-      // doc.data() will be undefined in this case
-      console.log("No such document - username!");
-    }
+  //   if (docSnap.exists()) {
+  //     setUsername(docSnap.data()?.username);
+  //     console.log("Document data:", docSnap.data()?.username);
+  //   } else {
+  //     setUsername(null);
+  //     // doc.data() will be undefined in this case
+  //     console.log("No such document - username!");
+  //   }
+  // }
+
+  async function getUserData(user: any) {
+    const unsub = onSnapshot(doc(firestore, "users", user.uid), (doc) => {
+      console.log("Current data: ", doc.data());
+
+      if (doc.data()) {
+        setUsername(doc.data()?.username);
+        console.log("Document data:", doc.data()?.username);
+      } else {
+        setUsername(null);
+        // doc.data() will be undefined in this case
+        console.log("No such document - username!");
+      }
+    });
   }
 
   return { user, username };
--- src/pages/enter.tsx
+++ src/pages/enter.tsx
@@ -1,11 +1,14 @@
 import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
-import { auth, googleProvider } from '../../lib/firebaseInit'
-import React, { useContext } from 'react'
+import { auth, firestore, googleProvider } from '../../lib/firebaseInit'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 import { UserContext } from 'lib/context'
+import { doc, getDoc, writeBatch } from 'firebase/firestore'
+import debounce from 'lodash.debounce'
 
 export default function Enter() {
-  const user = null
-  const { username } = useContext(UserContext)
+  const { user, username } = useContext(UserContext)
+  console.log("🚀 ~ file: enter.tsx:10 ~ Enter ~ username:", username)
+  console.log("🚀 ~ file: enter.tsx:10 ~ Enter ~ user:", user)
 
   // Sign in with Google button
   // Component SignInButton
@@ -16,19 +19,9 @@ export default function Enter() {
           .then((result) => {
             // This gives you a Google Access Token. You can use it to access the Google API.
             const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential?.accessToken;
-            // The signed-in user info.
-            const user = result.user;
             // ...
           }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
+            console.log(error)
           });
       } catch (error) {
         console.log(error)
@@ -44,9 +37,8 @@ export default function Enter() {
 
 
 
-  // Sign out button
+  // Sign out button Component
   function SignOutButton() {
-
     async function logout() {
       signOut(auth).then(() => {
         // Sign-out successful.
@@ -56,7 +48,6 @@ export default function Enter() {
       });
     }
 
-
     return (
       <button onClick={logout}>
         Sign Out
@@ -64,8 +55,143 @@ export default function Enter() {
     )
   }
 
-  // 
 
+  // Username form Component
+  function UsernameForm() {
+
+    const [formValue, setFormValue] = useState('')
+    const [isValid, setIsValid] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+    const { user, username } = useContext(UserContext)
+
+
+    // 1° check length of username
+    // 2° check if username is alphanumeric
+    // 3° check if username is available in firestore
+
+
+
+    function onChange(e: any) {
+      const value = e.target.value.toLowerCase()
+      const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+      if (value.length < 3) {
+        setFormValue(value)
+        setLoading(false)
+        setIsValid(false)
+      }
+
+      if (re.test(value)) {
+        setFormValue(value)
+        setLoading(true)
+        setIsValid(false)
+      }
+    }
+
+
+
+    useEffect(() => {
+      checkUsername(formValue)
+    }, [formValue])
+
+
+    const checkUsername = useCallback(
+      debounce(async (username: any) => {
+        if (username.length >= 3) {
+          const docRef = doc(firestore, "username", username);
+          const docSnap = await getDoc(docRef);
+
+          if (docSnap.exists()) {
+            console.log('Firestore read')
+            setIsValid(false)
+            setLoading(false)
+          } else {
+            setIsValid(true)
+            setLoading(false)
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+            console.log('Firestore read')
+          }
+
+        }
+      }, 500),
+      []
+    )
+
+
+
+    async function onSubmit(e: any) {
+      e.preventDefault()
+
+
+      // Get a new write batch
+      const batch = writeBatch(firestore);
+
+      const userRef = doc(firestore, "users", user.uid);
+      const usernameRef = doc(firestore, "username", formValue);
+
+
+      try {
+        batch.set(userRef, {
+          uid: user.uid,
+          username: formValue,
+          photoURL: user.photoURL,
+          displayName: user.displayName
+        });
+
+        batch.set(usernameRef, { uid: user.uid });
+
+        // Commit the batch
+        await batch.commit();
+      } catch (error) {
+        console.log(error)
+      }
+
+    }
+
+
+    return (
+      !username && (
+        <section>
+          <h3>Choose Username</h3>
+          <form onSubmit={onSubmit}>
+            <input type={'text'} value={formValue} onChange={onChange} placeholder="username" name="username" />
+
+            <UsernameMessage username={formValue} isValid={isValid} loading={loading} />
+
+            <button type={'submit'} className="btn-green" disabled={!isValid}>
+              Choose
+            </button>
+
+            <h3>Debug State</h3>
+            <div>
+              Username: {formValue}
+              <br />
+              Loading: {loading.toString()}
+              <br />
+              isValid: {isValid.toString()}
+            </div>
+          </form>
+
+        </section>
+      )
+    )
+  }
+
+
+  function UsernameMessage({ username, isValid, loading }: any) {
+
+    if (loading) {
+      return <p>Checking...</p>
+    } else if (isValid) {
+      return <p className="text-success">{username} is available!</p>
+    } else if (username && !isValid) {
+      return <p className="text-danger">That username is taken! :(</p>
+    } else {
+      return <p></p>
+    }
+  }
 
 
   // 1° user signed out <SignInButton />
@@ -76,10 +202,9 @@ export default function Enter() {
       {
         user ? (
           !username ? (
-            // tem usuario, mas nao tem username
+            // @ts-ignore
             <UsernameForm />
           ) : (
-            // tem usuario, e tem username
             <SignOutButton />
           )
         ) : (
